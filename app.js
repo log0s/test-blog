7d8a4d2abe93a37dcf9a72bd99448143028b8f77
log0s <JewishRhino@gmail.com>
2013-11-09 16:40:56 -0700

    Added support for creating blog posts

--- app.js
+++ app.js
@@ -41,4 +41,17 @@ app.get('/', function(req, res) {
     });
 });
 
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.jade', { title: 'New Post' } );
+});
+
+app.post('/blog/new', function(req, res) {
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function(error, docs) {
+        res.redirect('/')
+    });
+});
+
 app.listen(3000);
