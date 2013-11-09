e739a6b9196bf4060fdc62000bb56f49ecccb6cd
log0s <JewishRhino@gmail.com>
2013-11-09 16:58:36 -0700

    Refactored app.js to work with mongoDB

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-var articleProvider = new ArticleProvider();
+var articleProvider = new ArticleProvider('localhost', 27017);
 
 app.get('/', function(req, res) {
     articleProvider.findAll(function(error, docs) {
@@ -54,4 +54,24 @@ app.post('/blog/new', function(req, res) {
     });
 });
 
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show.jade', {
+                    title: article.title,
+                    article: article
+                   });
+    });
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
+
 app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
